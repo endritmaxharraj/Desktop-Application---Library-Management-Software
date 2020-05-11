94545037edd7087e5aa50edb25625d00132e2e36
Endrit Maxharraj <45464262+endritmaxharraj@users.noreply.github.com>
2020-05-11 04:28:22 +0200

    Sherben per te tableview toDoList.

---
+++ src/main/ToDoList.java
@@ -0,0 +1,40 @@
+
+
+
+public class ToDoList{
+	
+		private int todoList;
+		private DatePicker datatodoList;
+		private String teksti;
+		
+		public ToDoList(int todoList,DatePicker datatodoList,String teksti){
+			this.todoList = todoList;
+			this.datatodoList = datatodoList;
+			this.teksti = teksti;
+		}
+		
+		public int getToDoList(){
+			return getToDoList;
+		}
+		public DatePicker getDatatoDoList(){
+			return datatodoList;
+		}
+		public String getTeksti(){
+			return getTeksti;
+		}
+		
+		public void setTodoList(int todoList){
+			this.todoList = todoList;
+		}
+		public void setDatatoDoList(DatePicker datatodoList){
+			this.datatodoList = datatodoList;
+		}
+		public void  setTeksti(String teksti){
+			this.teksti = teksti;
+		}
+		
+		//me von e shoh per String toSring()..
+}
+
+			
+			
