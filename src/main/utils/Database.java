c94a2ee3cbb10d5212d183fc58794169afcbd7f1
endritbytyqi <58044635+endritbytyqi@users.noreply.github.com>
2020-06-27 23:48:59 +0200

    Add files via upload
    Databaza per Todolist, mbetet me u rregullu me databazen kryesore

---
+++ src/main/utils/Database.java
@@ -0,0 +1,56 @@
+package todolist;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Database {
+	private final StringProperty id;
+	private final StringProperty date;
+	private final StringProperty text;
+
+	
+	
+	public Database(String id,String date,String text)
+	{
+		this.id=new SimpleStringProperty(id);
+		this.date=new SimpleStringProperty(date);
+		this.text=new SimpleStringProperty(text);
+		
+	
+		
+		
+	}
+	public String getid() {return id.get();}
+	public String getdate() {return date.get();}
+	public String gettext() {return text.get();}
+	
+	
+	public void setid(String value)
+	{
+		id.set(value);
+	}
+	public void setdate(String value)
+	{
+		date.set(value);
+	}
+	public void settext(String value)
+	{
+		text.set(value);
+	}
+
+	public StringProperty idProperty()
+	{
+		return id;
+	}
+	public StringProperty dateProperty()
+	{
+		return date;
+	}
+	public StringProperty textProperty()
+	{
+		return text;
+	}
+
+
+}
+
